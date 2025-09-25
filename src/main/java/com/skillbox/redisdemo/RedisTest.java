10058d568c8935896d13e72cc5342c375f3f7299
Sergey Nazarov <ric19902002@icloud.com>
2025-09-25 06:13:09 +0000

    Update RedisTest.java

--- src/main/java/com/skillbox/redisdemo/RedisTest.java
+++ src/main/java/com/skillbox/redisdemo/RedisTest.java
@@ -8,20 +8,13 @@ import static java.lang.System.out;
 
 public class RedisTest {
 
-    // Запуск докер-контейнера:
-    // docker run --rm --name skill-redis -p 127.0.0.1:6379:6379/tcp -d redis
-
-    // Для теста будем считать неактивными пользователей, которые не заходили 2 секунды
     private static final int DELETE_SECONDS_AGO = 2;
-
-    // Допустим пользователи делают 500 запросов к сайту в секунду
+    
     private static final int RPS = 500;
 
-    // И всего на сайт заходило 1000 различных пользователей
     private static final int USERS = 1000;
 
-    // Также мы добавим задержку между посещениями
-    private static final int SLEEP = 1; // 1 миллисекунда
+    private static final int SLEEP = 1;
 
     private static final SimpleDateFormat DF = new SimpleDateFormat("HH:mm:ss");
 
@@ -35,12 +28,11 @@ public class RedisTest {
         RedisStorageQueue redis = new RedisStorageQueue();
         redis.init();
         Random random = new Random();
-        // Эмулируем 10 секунд работы сайта
+       
         while (true) {
             String user = redis.showNextUser();
             out.println("Показан на главной: " + user);
 
-            // 1 из 10 случаев — платёж
             if (random.nextInt(10) == 0) {
                 int lucky = random.nextInt(20) + 1;
                 String luckyUser = "user_" + lucky;
